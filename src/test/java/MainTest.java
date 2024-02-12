f60f01fbfd616d50c079b5f03f5d0c947df6ac2c
mahicks5 <102074697+mahicks5@users.noreply.github.com>
2024-02-12 11:05:24 -0700

    added basic template for unit tests

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Before
+    public void setUp() throws Exception {
+        Main.main(new String[0]);
+    }
+
+    @Test
+    public void testMain() {
+    }
+}
