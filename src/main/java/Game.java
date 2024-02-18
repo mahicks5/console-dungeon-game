f7637641efddeae6b85e262a2dc910973c9eb9b9
mahicks5 <102074697+mahicks5@users.noreply.github.com>
2024-02-17 21:22:27 -0700

    add basic game start and weapon factory test case

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,32 @@
-public class Main {
+import java.util.Scanner;
+
+public class Game {
+    public static void introMessage() {
+        System.out.println();
+        System.out.println("*************************");
+        System.out.println("*************************");
+        System.out.println("RPG Simulator");
+        System.out.println("*************************");
+    }
+
+    public static void promptCreateCharacter(Scanner userInput) {
+        System.out.println();
+
+        // ask user to name their character
+        System.out.println("enter character name:");
+        System.out.println();
+        String characterName = userInput.nextLine();
+
+        // ask user to choose a class
+        System.out.println();
+        System.out.println("enter character name:");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner userInput = new Scanner(System.in);
+
+        introMessage();
+        promptCreateCharacter(userInput);
+
+        userInput.close();
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,12 +4,38 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class MainTest {
+    WeaponFactory weaponFactory;
+
     @Before
     public void setUp() throws Exception {
-        Main.main(new String[0]);
+        weaponFactory = new WeaponFactory();
     }
 
     @Test
-    public void testMain() {
+    public void testWeaponFactoryWeaponTypes() {
+        Weapon swordTest = weaponFactory.makeWeapon("Sword");
+        swordTest.setName("Apothican Sword");
+        swordTest.setAdditionalAttack(10);
+
+        Weapon staffTest = weaponFactory.makeWeapon("Staff");
+        staffTest.setName("Staff of Ice");
+        staffTest.setAdditionalAttack(10);
+
+
+        Weapon bowTest = weaponFactory.makeWeapon("Bow");
+        bowTest.setName("Wrath of the Ancients");
+        bowTest.setAdditionalAttack(10);
+
+        assertEquals(Sword.class, swordTest.getClass());
+        assertEquals(Staff.class, staffTest.getClass());
+        assertEquals(Bow.class, bowTest.getClass());
+
+        assertEquals(swordTest.getName(), "Apothican Sword");
+        assertEquals(staffTest.getName(), "Staff of Ice");
+        assertEquals(bowTest.getName(), "Wrath of the Ancients");
+
+        assertEquals(swordTest.getAttack(), 13.0, 0.0);
+        assertEquals(staffTest.getAttack(), 14.0, 0.0);
+        assertEquals(bowTest.getAttack(), 12.0, 0.0);
     }
 }
